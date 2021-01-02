0116326b4c52174bf4d57a4b1cbec0499b0a6e35
Marcin <marcinu456@gmail.com>
2021-01-02 16:07:24 +0100

    lista 7 zad 15

--- Zad15/main.cpp
+++ Zad15/main.cpp
@@ -2,115 +2,121 @@
 #include <thread>
 #include <functional>
 #include <mutex>
-#include <string>
 #include <vector>
 #include <numeric>
 
 
-class Synchro {
-    std::vector<std::function<double()>> taskList; 
-    std::vector<double> results; 
-    std::vector<std::thread> threads; 
-    void calculi();
-    std::mutex taskMutex;
-    std::mutex resultMutex;
-    std::mutex is_ready_mutex;
-    bool is_ready = false;
-    std::mutex mutex;
-    std::condition_variable cv;
+class Synchro
+{
+	std::vector<std::thread> threads;
+	std::vector<std::function<double()>> taskList;
+	std::vector<double> results;
+
+	std::mutex resultMutex; //mutex on vector of results
+	std::mutex tasksMutex; //mutex on vector of taskList
+
+	std::atomic<bool> bIs_ready = false;
+
 public:
-    Synchro(int cores);
-    ~Synchro();
-    void add_task(std::function<double()>);
-    double average();
-    void stop();
+	Synchro(size_t threads_count)
+	{
+		for (size_t i = 0; i < threads_count; i++)
+		{
+			threads.push_back(std::thread(&Synchro::calculis, this));
+		}
+
+	}
+	~Synchro() {};
+
+	void add_task(std::function<double()> task)
+	{
+		std::lock_guard<std::mutex> lock(tasksMutex);
+		taskList.push_back(task);
+
+	}
+
+	double  avarage()
+	{
+		std::lock_guard<std::mutex> rlock(resultMutex);
+		double sumOfResults = std::accumulate(results.begin(), results.end(), decltype(results)::value_type(0));
+		
+		return (sumOfResults / results.size());
+	}
+
+	void stop()
+	{
+		bIs_ready = true;
+		std::unique_lock<std::mutex> tlock(tasksMutex);
+		while (taskList.size())
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		tlock.unlock();
+		for (auto& thread : threads)
+		{
+			thread.join();
+		}
+
+		threads.clear();
+	}
+private:
+
+	std::function<double()> getTask()
+	{
+		std::lock_guard<std::mutex> lock(tasksMutex);
+		if (taskList.empty())
+		{
+			return std::function<double()>();
+		}
+		else
+		{
+			std::function<double()> task = taskList.front();
+			taskList.erase(taskList.begin());
+			return task;
+		}
+
+	}
+
+
+	void calculis()
+	{
+		while (!bIs_ready)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			
+			std::function<double()>  task = getTask();
+			if (task)
+			{
+				
+				std::unique_lock<std::mutex> rlock(resultMutex);
+				results.push_back(task());
+				rlock.unlock();
+			}
+		}
+
+	}
 };
 
-Synchro::Synchro(int cores)
-{
-    threads.reserve(cores);
-    for (int i = 0; i < cores; i++) {
-        threads.push_back(std::thread(&Synchro::calculi, std::move(this)));
-    }
-}
-void Synchro::add_task(std::function<double()> task) {
-    std::lock_guard<std::mutex> lock(taskMutex);
-    taskList.push_back(task);
-    cv.notify_one();
-
-}
-double Synchro::average() {
-    if (results.size() != 0) {
-        std::lock_guard<std::mutex> lock(resultMutex);
-        double sum_of_elems = std::accumulate(results.begin(), results.end(), decltype(results)::value_type(0));
-        return sum_of_elems / results.size();
-    }
-    else return 0.0;
-}
-void Synchro::stop() {
-    if (threads.size() > 0) {
-        std::unique_lock<std::mutex> lock(is_ready_mutex);
-        is_ready = true;
-        lock.unlock();
-        cv.notify_all();
-        for (size_t i = 0; i < threads.size(); i++) {
-            threads[i].join();
-        }
-        
-        threads.clear();
-        
-    }
-}
-
-
-void Synchro::calculi() {
-
-    std::unique_lock<std::mutex> mlock(mutex);
-    while (!is_ready || taskList.size() > 0) {
-        if (taskList.size() > 0) {
-           std::unique_lock<std::mutex> tlock(taskMutex);
-
-           std::function<double()> task = taskList[0];
-
-           taskList.erase(taskList.begin());
-
-           tlock.unlock();
-           double result = task();
-           std::unique_lock<std::mutex> rlock(resultMutex);
-           results.push_back(result);
-           rlock.unlock(); 
-
-        }
-        else {
-            cv.wait(mlock);
-        }
-    }
-    mlock.unlock();
-}
-
-Synchro::~Synchro()
-{
-    stop();
-}
-
-
 double test() {
-    static double i = -1;
-    i++;
-    return 2+i;
+	static double i = 0;
+	i++;
+	return 2 * (i);	
 }
 
+int main()
+{
+	size_t n = 40;
+	Synchro synchro(105);
+	for (size_t i = 0; i < n; i++)
+	{
+		synchro.add_task(test);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
 
-int main() {
-    int n = 5;
-    Synchro synchro(n);
+	synchro.stop();
+	std::cout << "avarage: " << synchro.avarage() << std::endl;
 
-    for (int i = 0; i < n; i++) {
-        synchro.add_task(test);
-    }
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "average: " << synchro.average() << "\n"; 
-    synchro.stop();
-    return 0;
+	return 0;
 }
+
